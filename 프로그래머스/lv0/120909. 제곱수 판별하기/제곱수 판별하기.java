b8daab6bb24b805bcac91f3dfae0fc6b5cc17da5
ngotic <120378741+ngotic@users.noreply.github.com>
2023-02-23 15:59:38 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+            int answer = 0;
+	        int a =1;
+	        
+	        //12보다 작아져
+	        while( n/a > a ) {
+	        	a++;
+	        }
+	        if(n % a ==0)
+	        	return answer = 1;
+	        else 
+	        	return answer = 2;
+    }
+}
