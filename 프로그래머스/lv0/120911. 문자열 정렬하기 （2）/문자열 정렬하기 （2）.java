d0ee23709380dd272e0011f135c471b908fded92
ngotic <120378741+ngotic@users.noreply.github.com>
2023-02-23 15:40:07 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.44 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,25 +1,28 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string) {
-            char[] array = new char[my_string.length()];
-	        String temp = my_string.toLowerCase();
-	        System.out.println(temp);
+//             char[] array = new char[my_string.length()];
+// 	        String temp = my_string.toLowerCase();
+// 	        System.out.println(temp);
 	        
-	        for(int i=0; i< my_string.length(); i++) {
-	        	array[i] = temp.charAt(i);
-	        }
+// 	        for(int i=0; i< my_string.length(); i++) {
+// 	        	array[i] = temp.charAt(i);
+// 	        }
 	        
 	        
-	        for(int i=0 ; i< my_string.length()-1 ;i++) { // 버블 소트
-	        	for(int j=0; j < my_string.length()-1-i; j++) { // 
-	        		if ( array[j] > array[j+1] ) {
-	        			char tempchar =  array[j];
-	        			array[j] = array[j+1];
-	        			array[j+1] = tempchar;	
-	        		}
-	        	}
+// 	        for(int i=0 ; i< my_string.length()-1 ;i++) { // 버블 소트
+// 	        	for(int j=0; j < my_string.length()-1-i; j++) { // 
+// 	        		if ( array[j] > array[j+1] ) {
+// 	        			char tempchar =  array[j];
+// 	        			array[j] = array[j+1];
+// 	        			array[j+1] = tempchar;	
+// 	        		}
+// 	        	}
 	        	
-	        }
-
+// 	        }
+            char [] array=my_string.toLowerCase().toCharArray();
+		  	Arrays.sort(array);
 	        return new String(array);
     }
 }
