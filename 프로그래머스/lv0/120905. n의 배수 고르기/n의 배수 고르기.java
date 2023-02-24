2d167ee395fc08b1117d45854767144cba440f28
ngotic <120378741+ngotic@users.noreply.github.com>
2023-02-24 23:58:08 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+        
+        int cnt = 0;
+        for(int i=0; i<numlist.length ; i++){
+            if(numlist[i]%n==0){
+                numlist[cnt] =numlist[i];
+                cnt++;
+            }
+        }
+        answer = new int[cnt];
+        for(int i=0; i<cnt; i++){
+            answer[i] = numlist[i];
+        }
+        return answer;
+    }
+}
