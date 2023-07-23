d16331a2fa2ac73e8e547c6afb8d6e0a2cf3ae05
ngotic <120378741+ngotic@users.noreply.github.com>
2023-07-23 23:29:26 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 212 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// 순서를 바꾸면 동일? > 교집합 찾기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// 알파벳 자체가 26개 
+		int [] cntA = new int[26];
+		int [] cntB = new int[26];
+		
+		String first = sc.next();
+		String second = sc.next();
+		
+		for(char a: first.toCharArray()) {
+			cntA[a-'a']++;
+		}
+		
+		for(char b: second.toCharArray()) {
+			cntB[b-'a']++;
+		}
+		int abs=0;
+		for(int i=0; i< 26; i++)
+			abs += Math.abs(cntA[i]-cntB[i]);
+		
+		System.out.println(abs);
+	}
+	
+}
